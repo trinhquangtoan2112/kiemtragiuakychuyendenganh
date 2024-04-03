5a1b0011be71929c450951f6b793269bf9d9b508
trinhquangtoan2112 <trinhtoanquang@gmail.com>
2024-04-03 17:24:32 +0700

    lan2 2

--- TrinhQuangToan.py
+++ TrinhQuangToan.py
@@ -1,2 +1,16 @@
 # đọc một file văn bản thành nhiều dòng
 # Ghi ra màn hình theo  thứ tự ngược lại của chungs
+
+def reverse_display(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+            for line in reversed(lines):
+                print(line.strip())
+    except FileNotFoundError:
+        print("Không tìm thấy tệp.")
+
+file_path = 'C:\\Users\\Admin\\Desktop\\bailam\\Test.txt'
+reverse_display(file_path)
+  
+#hello trinh quang toan 20A10010263
